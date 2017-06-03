ace338951c7fd87f05b042d7be8c093e45d9df01
JohannesTaelman <johannes.taelman@gmail.com>
2017-06-03 23:45:46 +0200

    travis build fix (2)

--- firmware/compile_firmware_linux.sh
+++ firmware/compile_firmware_linux.sh
@@ -4,7 +4,7 @@ set -e
 export PATH=${axoloti_runtime}/platform_linux/gcc-arm-none-eabi-6-2017-q1-update/bin:${axoloti_runtime}/platform_linux/bin:$PATH
 
 echo $PATH
-ls 
+ls ${axoloti_runtime}/platform_linux/gcc-arm-none-eabi-6-2017-q1-update/bin
 
 cd "${axoloti_firmware}"
 make -f Makefile.patch clean
--- firmware/compile_firmware_osx.sh
+++ firmware/compile_firmware_osx.sh
@@ -3,6 +3,9 @@ set -e
 
 export PATH=${axoloti_runtime}/platform_osx/gcc-arm-none-eabi-6-2017-q1-update/bin:${axoloti_runtime}/platform_osx/bin:$PATH
 
+echo $PATH
+ls ${axoloti_runtime}/platform_linux/gcc-arm-none-eabi-6-2017-q1-update/bin
+
 echo "Compiling firmware... ${axoloti_firmware}"
 cd "${axoloti_firmware}"
 make -f Makefile.patch clean
